41954e174d2dabbaee1d6e1f24375ff889aa7211
Anastasiia Holovachova <a.holovachova@gmail.com>
2018-12-26 00:32:39 +0100

    corrected dom list of items

--- Documents/zadania_domowe/js/10-dom/js/main.js
+++ Documents/zadania_domowe/js/10-dom/js/main.js
@@ -2,9 +2,9 @@ let btnClick = document.getElementsByTagName("button")
 
 
 function myFunction() {
-    let n = document.getElementsByTagName("LI").length;
+    let n = document.getElementsByTagName("LI").length+1;
     let x = document.createElement("LI");
-    let t = document.createTextNode("Item");
+    let t = document.createTextNode("Item"+" "+n);
     x.appendChild(t);
     document.getElementById("items").appendChild(x);
 }
